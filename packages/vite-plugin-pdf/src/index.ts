905d6269c274d475492cf562aab7b36abebb673f
XLor <yjl9903@vip.qq.com>
2022-09-05 01:42:52 +0800

    chore: init plugin

--- packages/vite-plugin-pdf/src/index.ts
+++ packages/vite-plugin-pdf/src/index.ts
@@ -1,3 +1,9 @@
-export function hello() {
-  return 'world';
+import type { Plugin } from 'vite';
+
+export default function PDF(): Plugin {
+  return {
+    name: 'vite-plugin-pdf',
+    apply: 'build',
+    async closeBundle() {}
+  };
 }
